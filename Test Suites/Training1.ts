47b073e1158872ef2480e9dd8e581f2dbb8baab4
tannieding <90038886+tannieding@users.noreply.github.com>
2021-09-10 18:52:16 +0700

    auto1

---
+++ Test Suites/Training1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Training1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ce71ada3-a714-4ca1-8a72-590903404c76</testSuiteGuid>
+   <testCaseLink>
+      <guid>213a99ce-b520-4bfc-be5d-6e814dd3c7db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Training1</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>12eb8ba4-174d-4fb1-b724-4a566231b48f</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
